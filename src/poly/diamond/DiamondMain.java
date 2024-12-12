4ba5c467371e96900a07ae077289ac524900e02e
JiHo Jeon <wjswlgh96@naver.com>
2024-12-12 19:00:23 +0900

    다형성 끝

---
+++ src/poly/diamond/DiamondMain.java
@@ -0,0 +1,13 @@
+package poly.diamond;
+
+public class DiamondMain {
+
+    public static void main(String[] args) {
+        Child a = new Child();
+        a.methodA();
+        a.methodB();
+
+        a.methodCommon();
+
+    }
+}
